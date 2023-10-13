20eca3018396af7f6fedffd1d0c9c1258e1dadb3
LQ458 <yihao.qin17311-biph@basischina.com>
2023-10-13 16:11:34 +0800

    Update post.js

--- models/post.js
+++ models/post.js
@@ -18,7 +18,10 @@ const postSchema = new Schema ({
         type: Date,
         default: Date.now,
         get: function(value) {
-            return value.toLocaleString([], { dateStyle: 'short', timeStyle: 'short' });
+            var offset = 8; // UTC +8
+            var utc = value.getTime() + (value.getTimezoneOffset() * 60000); // 转为 UTC 时间
+            var date = new Date(utc + (3600000 * offset)); // 根据偏移量调整时间
+            return date.toLocaleString([], { dateStyle: 'short', timeStyle: 'short' });
         },
         immutable: true,
     },
