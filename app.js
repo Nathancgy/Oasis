995dbf3bfd0234ed21845b21378381912adbd87e
LQ <yihao.qin17311-biph@basischina.com>
2023-10-14 14:32:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.set('view engine', 'ejs');
-app.listen(80, '0.0.0.0', () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log("Server running on http");
 });
 
