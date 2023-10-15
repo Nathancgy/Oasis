1ca70e3042cd83ffb51efea771c8f35779a24f9b
LQ458 <yihao.qin17311-biph@basischina.com>
2023-10-15 12:39:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -115,6 +115,9 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+app.get('/verification', (req,res) =>{
+    res.render('verification');
+})
 
 app.post('/login', passport.authenticate('local', {
     successRedirect: 'dashboard',
